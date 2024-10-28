853ec41a187db4fcc6b45cf253d293948c65e96b
hemodious <hemodious@gmail.com>
2024-10-28 12:22:04 +0000

    Add files via upload

---
+++ one/sort.py
@@ -0,0 +1,13 @@
+import random
+import string
+characters=string.ascii_letters+ string.digits*4
+
+ans=''.join(random.choices(characters, k=7) )
+store=[] 
+for good in store:
+    if store == ans :
+        print("already exists")
+    else:
+        store.append(ans)  
+        print(ans)   
+print(ans)

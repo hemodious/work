18be53a8503b0ae46ab214887cae2f590d6403d4
hemodious <hemodious@gmail.com>
2024-11-17 18:25:21 +0000

    l

--- one/app.py
+++ one/app.py
@@ -1,4 +1,5 @@
 
+import os
 from flask_socketio import  SocketIO, send
 from flask_cors import  CORS
 from my_module import chat_connection
@@ -30,4 +31,4 @@ def handlemessages(data):
 
 if __name__ == '__main__':
     #
-    socketio.run(app,debug=True)
+   socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)

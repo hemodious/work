fe6284e391606cddf808514bac43de6e0cb7cc8f
Kekoom <ekelvinkoomson101@gmail.com>
2024-11-28 13:04:06 +0000

    fix

--- one/static/index.js
+++ one/static/index.js
@@ -49,36 +49,18 @@ const selectedCategory = document.querySelector('input[name="report-reason"]:che
     body: formData
   })
   .then(response => {
+    console.log(response)
     if (!response.ok) {
       throw new Error('Network response was not ok ' + response.statusText);
     }
     return response.json();
   })
   .then(data => {
+
     
     window.location.href = '/success'; // Relative path to the success page
     
     document.getElementById('response').innerText = 'Success: ' + JSON.stringify(data);
-   
-  })
-  .catch((error) => {
-    console.error('Error:', error);
-    document.getElementById('response').innerText = 'Error: ' + error;
-  });
- 
-  fetch(' http://127.0.0.1:5000/user', {
-    method: ' GET',
-    body: formData
-  })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok ' + response.statusText);
-    }
-    return response.json();
-  })
-  .then(data => {
-    
-    localStorage .setItem('complaint_id', data.complaint_id)
    
   })
   .catch((error) => {
